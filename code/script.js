238e7322f9d4cfff8176c0557698ebdc41191b01
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2025-02-14 00:17:52 -0800

    first commit

---
+++ code/script.js
@@ -0,0 +1,19 @@
+async function getAdvice() {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    console.log(data);
+    const idOfAdvice = data.slip.id;
+    const advice = data.slip.advice;
+    console.log(idOfAdvice);
+    console.log(advice);
+    const container = document.querySelector(".box");
+    container.innerHTML = `
+    <p class="adviceN">Advice #${idOfAdvice}</p>
+    <h1 class="advice"> “${advice}“</h1>
+    `;
+  } catch (error) {
+    alert(error.message);
+  }
+}
+getAdvice();
